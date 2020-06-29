70e2240c58cd3b19a372da1d6769fa995d8eb70d
Dean De Leo <deandeleo@hotmail.co.uk>
2020-06-29 12:28:46 +0200

    [Time] Move the time formatting into time.hpp

--- src/timer.cpp
+++ src/timer.cpp
@@ -17,104 +17,11 @@
 
 #include "timer.hpp"
 
-#include <cinttypes>
-#include <cmath>
-#include <sstream>
+#include "time.hpp" // common::time::to_string()
 
 using namespace std;
 using namespace std::chrono;
 using namespace common;
-
-template <typename D>
-static string to_nanoseconds(D duration){
-    stringstream result;
-    result << (uint64_t) duration_cast<chrono::nanoseconds>(duration).count() << " nanosecs";
-    return result.str();
-}
-
-template <typename D>
-static string to_microseconds(D duration){
-    uint64_t time_in_nanosecs = (uint64_t) duration_cast<chrono::nanoseconds>(duration).count();
-    uint64_t time_in_microsecs = time_in_nanosecs / 1000;
-
-    stringstream result;
-    if(time_in_microsecs >= 3){
-        result << time_in_microsecs << " microsecs";
-    } else {
-        char buffer[128];
-        snprintf(buffer, 128, "%.3d", (int) (time_in_nanosecs % 1000));
-        result << time_in_microsecs << "." << buffer << " microsecs";
-    }
-
-    return result.str();
-}
-
-template <typename D>
-static string to_milliseconds(D duration){
-    uint64_t time_in_microsecs = (uint64_t) duration_cast<chrono::microseconds>(duration).count();
-    uint64_t time_in_millisecs = time_in_microsecs / 1000;
-
-    stringstream result;
-    if(time_in_microsecs >= 3){
-        result << time_in_millisecs << " milliseconds";
-    } else {
-        char buffer[128];
-        snprintf(buffer, 128, "%.3d", (int) (time_in_microsecs % 1000));
-        result << time_in_millisecs << "." << buffer << " milliseconds";
-    }
-
-    return result.str();
-}
-
-template <typename D>
-static string to_seconds(D duration){
-    uint64_t time_in_millisecs = (uint64_t) duration_cast<chrono::milliseconds>(duration).count();
-    uint64_t time_in_seconds = time_in_millisecs / 1000;
-
-    stringstream result;
-//    if(time_in_seconds >= 10){
-//        result << time_in_seconds << " seconds";
-//    } else {
-        char buffer[128];
-        snprintf(buffer, 128, "%.3d", (int) (time_in_millisecs % 1000));
-        result << time_in_seconds << "." << buffer << " seconds";
-//    }
-
-    return result.str();
-}
-
-template <typename D>
-static string to_minutes(D duration){
-    uint64_t seconds = ((uint64_t) duration_cast<chrono::seconds>(duration).count()) % 60ull;
-    uint64_t minutes = (uint64_t) duration_cast<chrono::minutes>(duration).count();
-
-    char buffer[128];
-    snprintf(buffer, 128, "%" PRIu64 ":%02" PRIu64 " minutes", minutes, seconds);
-    return string(buffer);
-}
-
-template <typename D>
-static string to_hours(D duration){
-    uint64_t seconds = ((uint64_t) duration_cast<chrono::seconds>(duration).count()) % 60ull;
-    uint64_t minutes = (uint64_t) duration_cast<chrono::minutes>(duration).count() % 60ull;
-    uint64_t hours = (uint64_t) duration_cast<chrono::hours>(duration).count();
-
-    char buffer[128];
-    snprintf(buffer, 128, "%" PRIu64 ":%02" PRIu64 ":%02" PRIu64 " hours", hours, minutes, seconds);
-    return string(buffer);
-}
-
-template <typename D>
-static string to_days(D duration){
-    uint64_t seconds = ((uint64_t) duration_cast<chrono::seconds>(duration).count()) % 60ull;
-    uint64_t minutes = (uint64_t) duration_cast<chrono::minutes>(duration).count() % 60ull;
-    uint64_t hours = (uint64_t) duration_cast<chrono::hours>(duration).count() % 24ull;
-    uint64_t days = (uint64_t) duration_cast<chrono::hours>(duration).count() / 24;
-
-    char buffer[128];
-    snprintf(buffer, 128, "%" PRIu64 " day(s) and %" PRIu64 ":%02" PRIu64 ":%02" PRIu64 " hours", days, hours, minutes, seconds);
-    return string(buffer);
-}
 
 template <bool with_barrier>
 string Timer<with_barrier>::to_string() const{
@@ -129,24 +36,7 @@ string Timer<with_barrier>::to_string() const{
     // duration
     auto d = t1 - m_t0;
 
-    stringstream result;
-
-    uint64_t time_in_nanosecs = (uint64_t) duration_cast<chrono::nanoseconds>(d).count();
-    if(time_in_nanosecs <= 1000){
-        return to_nanoseconds(d);
-    } else if(time_in_nanosecs <= (uint64_t) pow(10, 6)){
-        return to_microseconds(d);
-    } else if(time_in_nanosecs <= (uint64_t) pow(10, 9)) {
-        return to_milliseconds(d);
-    } else if(time_in_nanosecs <= (uint64_t) pow(10, 9) * 90){ // 90 seconds
-        return to_seconds(d);
-    } else if(time_in_nanosecs < (uint64_t) pow(10, 9) * 60 * 60){
-        return to_minutes(d);
-    } else if(time_in_nanosecs < (uint64_t) pow(10, 9) * 60 * 60 * 24){
-        return to_hours(d);
-    } else {
-        return to_days(d);
-    }
+    return time::to_string( d );
 }
 
 template<bool use_barrier>
